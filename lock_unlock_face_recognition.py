948c801e5d2438afe438eebaa9aba78ec589470b
Ansh bhargava <69900837+Ansh04122001@users.noreply.github.com>
2023-04-13 19:18:05 +0530

    Update lock_unlock_face_recognition.py

--- lock_unlock_face_recognition.py
+++ lock_unlock_face_recognition.py
@@ -6,48 +6,28 @@ import ctypes
 import os
 import datetime
 import time
-
-
 def assure_path_exists(path):
     dir = os.path.dirname(path)
     if not os.path.exists(dir):
         os.makedirs(dir)
-
-
 counter_correct = 0  # counter variable to count number of times loop runs
 counter_wrong = 0
-
 now = datetime.datetime.now()  # extract current time
 now = now.second  # we need only seconds
-
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-
 assure_path_exists("dataset/")  # add path to the folder where all the files are saved
-
 recognizer.read('trainer.yml')  # load training model
-
 cascadePath = ("haarcascade_frontalface_default.xml")  # cascade path
-
 faceCascade = cv2.CascadeClassifier(cascadePath);  # load cascade
-
 font = cv2.FONT_HERSHEY_COMPLEX_SMALL  # Set the font style
-
 cam = cv2.VideoCapture(0)
-
 while True:
-
     ret, im = cam.read()
-
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-
     faces = faceCascade.detectMultiScale(gray, 1.3, 5)
-
     for (x, y, w, h) in faces:
-
         cv2.rectangle(im, (x - 20, y - 20), (x + w + 20, y + h + 20), (0, 255, 0), 4)
-
         Id, confidence = recognizer.predict(gray[y:y + h, x:x + w])  # Recognize the face belongs to which ID
-
         if (confidence > 80):  # confidence usually comes greater than 80 for strangers
             counter_wrong += 1
             print("Wrong")
@@ -64,7 +44,6 @@ while True:
             print("counter_correct - " + str(counter_correct))
             cv2.rectangle(im, (x - 22, y - 90), (x + w + 22, y - 22), (255, 255, 255), -1)
             cv2.putText(im, str(Id), (x, y - 40), font, 1, (0, 0, 0), 2)
-
         if (counter_wrong == 3):
             pyautogui.moveTo(48, 748)
             pyautogui.click(48, 748)
@@ -72,12 +51,8 @@ while True:
             cv2.destroyAllWindows()
             ctypes.windll.user32.LockWorkStation()
             sys.exit()
-
     cv2.imshow('Webcam', im)
-
     if cv2.waitKey(10) & 0xFF == ord('*'):  # If '*' is pressed, terminate the  program
         break
-
 cam.release()
-
 cv2.destroyAllWindows()
